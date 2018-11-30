7b63b84b76c97cd1f8bea8b7484a8dc5b9eda8b7
elwark97 <alexisrosero3@gmail.com>
2018-11-30 00:48:13 -0500

    TallerNodeJS

---
+++ src/app/controllers/newController.js
@@ -0,0 +1,79 @@
+
+const dbConnection= require('../../config/dbConnection');
+const conn= dbConnection();
+const controller= {};
+
+
+controller.list= (req,res)=>{
+conn.query('SELECT * FROM news', (err, result)=>{
+    if(err){
+        res.json(err);
+    }
+res.render ('news', {
+    news:result
+
+});
+});
+}
+
+controller.add = (req,res)=>{
+    const {title,news} =req.body;
+    
+        conn.query('INSERT INTO news SET ?',
+    {
+        title,
+        news
+    }, (err,result)=>{
+        res.redirect('/news');
+    });
+    };
+    
+
+    controller.eliminar = (req,res)=>{
+        
+
+        const id=req.params.id_noticia;
+            conn.query('DELETE from news where id_news = ?',
+       
+            id
+        
+        , (err,result)=>{
+            res.redirect('/news');
+        });
+    };
+
+
+    controller.updateNews= (req,res)=>{
+   
+        const id=req.params.id_news
+    
+        res.render ('Anoticias', {
+            actualizado:id
+        
+        });
+        
+    }
+    
+    controller.Actualizarnoti = (req,res)=>{
+        const {title,news,id_news} =req.body;
+        
+        
+            conn.query('UPDATE news SET title = ?, news = ? where id_news = ?',
+            [title, news,id_news]
+            
+            ,(err,result)=>{
+    
+            if(err){
+    
+            res.json(err);
+        }
+    
+            res.redirect('/news');
+           
+        });
+        
+    }
+    
+   
+ 
+module.exports= controller;
